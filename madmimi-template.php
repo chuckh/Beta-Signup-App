3c9c386538d0c00cbec06127254a9548c1032b90
Chuck Horton <chuckh@hware.com>
2014-09-02 07:19:51 -0700

    Added Firebase saving.

--- madmimi-template.php
+++ madmimi-template.php
@@ -1,11 +1,12 @@
 <?php 
 	$username = "name@example.com"; 
 	$api_key = "appkey value"; 
-	$email = $_GET['email'];; 
+	$email = $_GET['email'];
+	$create_at = $_GET['create_at'];
 	$listname = "Madmimi List Name"; 
 	$ch = curl_init('https://madmimi.com/audience_lists/'.$listname.'/add'); 
 	curl_setopt($ch, CURLOPT_POST, TRUE); 
-	curl_setopt($ch, CURLOPT_POSTFIELDS,"username=".$username."&api_key=".$api_key."&email=".$email); 
+	curl_setopt($ch, CURLOPT_POSTFIELDS,"username=".$username."&api_key=".$api_key."&email=".$email."&create_at=".$create_at); 
 	$response = curl_exec($ch); 
 	echo "Added: ", $email;
 ?>
