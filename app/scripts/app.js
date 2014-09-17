18d475c1ecddfb975b3d0725803d5d9d4f68bcbd
Chuck Horton <chuckh@hware.com>
2014-09-16 18:06:48 -0700

    Fixed Background images wording. Update promo text
    Bumped version number.

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -3,7 +3,7 @@
 
   document.addEventListener('polymer-ready', function() {
     // Perform some behaviour
-    console.log('Actionplanr Beta Invite is ready to rock! Version 1.1.0');
+    console.log('Actionplanr Beta Invite is ready to rock! Version 1.2.2');
   });
 
 // wrap document so it plays nice with other libraries
